5b546aa9e26ac1ecf6eeb207c5aec893f09541b1
Gabe Kugelmass <gkord@me.com>
2019-09-28 13:45:59 -0400

    added more JS functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const game = () => {
   let playerScore = 0;
   let compScore = 0;
 
+  //start our game
   const startGame = () => {
     //select intro button and store in a variable
     const playBtn = document.querySelector(".intro button");
@@ -14,12 +15,37 @@ const game = () => {
     //create event listener for playBtn
     playBtn.addEventListener("click", () => {
       introScreen.classList.add("fadeOut");
-      matchScreen.classList.add('fadeIn');
+      matchScreen.classList.add("fadeIn");
+    });
+  };
+
+  //play match
+  const playMatch = () => {
+    //select options buttons and store in a variable
+    const options = document.querySelectorAll(".options button");
+    //select the player hand and store in a variable
+    const playerHand = document.querySelector(".player-hand");
+    //select the computer hand and store in a variable
+    const computerHand = document.querySelectorAll("computer-hand");
+
+    //create an array that represents the computer's choices
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    //loop through each option button and add event listener to each button
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        //create a randomize method and store in a variable
+        const computerNumber = Math.floor(Math.random() * 3);
+        //apply randomize method to computer options array
+        const computerChoice = computerOptions[computerNumber];
+        console.log(computerChoice);
+      });
     });
   };
 
   //call startGame function
   startGame();
+  playMatch();
 };
 
 //call game function
